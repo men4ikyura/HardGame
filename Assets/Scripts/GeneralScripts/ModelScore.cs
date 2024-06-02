086019c91c0e59e8a447bf883393407599c6105e
zhilin yura <zhilin_yura001@vk.com>
2024-06-02 12:09:53 +0500

    modified script

--- Assets/Scripts/GeneralScripts/ModelScore.cs
+++ Assets/Scripts/GeneralScripts/ModelScore.cs
@@ -2,6 +2,6 @@ using UnityEngine;
 
 public class ModelScore : MonoBehaviour
 {
-    public static int score = 8;
+    public static int score = 5;
     public static void ReduceScore() =>  score -= 1;
 }
